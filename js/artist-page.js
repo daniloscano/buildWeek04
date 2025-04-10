6f73e05457241e8c8051a57a34ce9238eaae68f4
Andrea <andrew.ryan.r.a@gmail.com>
2025-04-10 21:35:59 +0200

    Artist-page: added popular songs section

--- js/artist-page.js
+++ js/artist-page.js
@@ -1,12 +1,28 @@
-const ENDPOINT = 'https://striveschool-api.herokuapp.com/api/deezer/artist';
+const ARTIST_API = 'https://striveschool-api.herokuapp.com/api/deezer/artist';
+const MUSIC_API = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=';
 const params = new URLSearchParams(location.search);
 const id = params.get('id');
 
 const artistContainer = document.getElementById('artistContainer');
+const hidePopularButton = document.querySelector('.hide-popular-songs');
+const firstPopularContainer = document.getElementById('firstPopularCtn');
+const secondPopularContainer = document.getElementById('secondPopularCtn');
+
+hidePopularButton.addEventListener('click', () => {
+    const isTextShowLess = hidePopularButton.innerHTML === 'Show less';
+
+    if (isTextShowLess) {
+        hidePopularButton.innerHTML = 'See more';
+        secondPopularContainer.classList.add('visually-hidden');
+    } else {
+        hidePopularButton.innerHTML = 'Show less';
+        secondPopularContainer.classList.remove('visually-hidden');
+    }
+});
 
 const getArtist = async () => {
     try {
-        const response = await fetch(`${ENDPOINT}/${79126582}`);
+        const response = await fetch(`${ARTIST_API}/${79126582}`);
         return response.json();
     } catch (error) {
         console.log(error);
@@ -45,4 +61,125 @@ const generateArtistHeader = data => {
 
 getArtist().then(results => generateArtistHeader(results));
 
-getArtist().then(results => console.log(results));
+// GENERATE TRACKLIST
+const getArtistTracklist = async () => {
+    try {
+        const response = await fetch(`${MUSIC_API}alexcherney`);
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const songDuration = time => {
+    const minutes = Math.trunc(time / 60);
+    const seconds = String(Math.trunc(time % 60)).padStart(2, 0);
+
+    return `${minutes}:${seconds}`;
+};
+
+const generateFavoriteSongButton = () => {
+    const plusIcon = '<i class="bi bi-plus-circle"></i>';
+    const favoriteIcon = '<i class="bi bi-check-circle-fill added-icon"></i>';
+
+    const icon = document.createElement('div');
+    icon.setAttribute('class', 'plus-icon');
+    icon.innerHTML = plusIcon;
+
+    icon.addEventListener('click', () => {
+        if (icon.innerHTML === favoriteIcon) {
+            icon.innerHTML = plusIcon;
+            icon.classList.remove('favorite');
+        } else {
+            icon.innerHTML = favoriteIcon;
+            icon.classList.add('favorite');
+        }
+    });
+
+    return icon;
+};
+
+const generateDropdownMenu = () => {
+    const icon = document.createElement('i');
+    icon.setAttribute('data-bs-toggle', 'dropdown');
+    icon.setAttribute('class', 'bi bi-three-dots dots-icon dropdown');
+
+    const ul = document.createElement('ul');
+    ul.setAttribute('class', 'dropdown-menu');
+
+    dropdownItems.forEach((item, i) => {
+        const li = document.createElement('li');
+
+        const a = document.createElement('a');
+        a.href = '#';
+        a.setAttribute('class', 'dropdown-item fst-normal');
+        a.innerHTML = `<span>${dropdownIcons[i]}${item}</span>`;
+
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    icon.appendChild(ul);
+    return icon;
+};
+
+const generatePopularSongs = (data, i, appender) => {
+    const { cover_big: cover, title } = data.album;
+    const time = songDuration(data.duration);
+    const plusIcon = generateFavoriteSongButton();
+    const dropdownMenu = generateDropdownMenu();
+
+    const listContainer = document.createElement('ul');
+    listContainer.setAttribute('class', 'tracklist-container mb-0');
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'tracklist');
+    document.addEventListener('click', e => {
+        const isTargetTracklist = e.target === li;
+        const isTargetOutsideTracklist = e.target !== li;
+        const isTargetDropdown = e.target === dropdownMenu;
+
+        if (isTargetTracklist || isTargetDropdown) {
+            li.classList.add('bg-lighter');
+            plusIcon.classList.add('show-icon');
+            dropdownMenu.classList.add('show-icon');
+        } else if (isTargetOutsideTracklist) {
+            li.classList.remove('bg-lighter');
+            plusIcon.classList.remove('show-icon');
+            dropdownMenu.classList.remove('show-icon');
+        }
+    });
+
+    const number = document.createElement('p');
+    number.setAttribute('class', 'song-number mb-0');
+    number.innerHTML = i.padStart(2, 0);
+
+    const albumCover = document.createElement('img');
+    albumCover.setAttribute('class', 'album-cover');
+    albumCover.src = cover;
+    albumCover.alt = title;
+
+    const songTitle = document.createElement('a');
+    songTitle.setAttribute('class', 'song-title');
+    songTitle.innerText = title;
+
+    const duration = document.createElement('p');
+    duration.setAttribute('class', 'song-duration mb-0');
+    duration.innerText = time;
+
+    duration.prepend(plusIcon);
+    duration.appendChild(dropdownMenu);
+    li.append(number, albumCover, songTitle, duration);
+    listContainer.appendChild(li);
+
+    appender.appendChild(listContainer);
+};
+
+getArtistTracklist().then(results => {
+    results.data.slice(0, 5).forEach((result, i) => {
+        generatePopularSongs(result, `${i + 1}`, firstPopularContainer);
+    });
+    results.data.slice(5, 10).forEach((result, i) => {
+        generatePopularSongs(result, `${i + 6}`, secondPopularContainer);
+    });
+});
--- js/icons-and-items.js
+++ js/icons-and-items.js
@@ -1,3 +1,23 @@
 const verifiedIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
 <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
 </svg>`;
+
+const dropdownItems = [
+    'Add to playlist',
+    'Save to your Liked Song',
+    'Go to song radio',
+    'Go to album',
+    'View credits',
+    'Share',
+    'Open in Desktop app',
+];
+
+const dropdownIcons = [
+    '<i class="bi bi-plus-lg"></i>',
+    '<i class="bi bi-plus-circle"></i>',
+    '<i class="bi bi-broadcast"></i>',
+    '<i class="bi bi-disc"></i>',
+    '<i class="bi bi-music-note-list"></i>',
+    '<i class="bi bi-box-arrow-up"></i>',
+    '<i class="bi bi-spotify"></i>',
+];
